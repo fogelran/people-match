8b217465b2c154ffc9ae3e894f310e2a1c53f0de
fogelran <fogelran@gmail.com>
2020-01-07 15:24:00 +0200

    Support for user questions.

--- src/people_search.py
+++ src/people_search.py
@@ -4,16 +4,20 @@ from .user import User
 class PeopleSearch:
     def __init__(self):
         self._users = {}
-        self._questions = []
+        self._questions = {}
 
     def add_user(self, name_id):
         if name_id not in self._users:
             self._users[name_id] = User()
+            self._questions[name_id] = []
         else:
             raise ValueError("Name '{0}' already exist".format(name_id))
 
     def get_user(self, name_id):
         return self._users[name_id]
 
-    def add_question(self, question):
-        self._questions.append(question)
+    def add_user_question(self, name_id, question):
+        self._questions[name_id].append(question)
+
+    def get_user_questions(self, name_id):
+        return self._questions[name_id]
--- test_people_search.py
+++ test_people_search.py
@@ -30,12 +30,21 @@ def test_when_requesting_non_exist_user_then_throw():
     # test
     people_search.get_user(name_id="Ran")
 
-    # assert
     with pytest.raises(KeyError):
         people_search.get_user(name_id="Jessie")
 
 
-def test_add_question():
+def test_when_adding_user_question_then_added():
+    # prepare
     people_search = PeopleSearch()
-    people_search.add_question(question="Do you like rainbows?")
+    people_search.add_user(name_id="Ran")
+
+    # test
+    people_search.add_user_question(name_id="Ran", question="Do you like rainbows?")
+    people_search.add_user_question(name_id="Ran", question="Do you like sex?")
+
+    # assert
+    assert len(people_search.get_user_questions(name_id="Ran")) == 2
+    with pytest.raises(KeyError):
+        people_search.add_user_question(name_id="Jessie", question="Do you like rainbows?")
 
